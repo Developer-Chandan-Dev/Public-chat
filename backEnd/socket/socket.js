2265f343709a806d11bc8046d85bdbd405fd7c4b
Chandan Dev <chandan.dev.pro@gmail.com>
2024-10-27 04:55:28 +0530

    Update socket.js

--- backEnd/socket/socket.js
+++ backEnd/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://chattervibe.netlify.app"],
+    origin: ["http://localhost:3000", "https://chattervibe.netlify.app","https://public-chat-ucdd.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
